3fd554913ea1e163acd0555478412f4fb2345010
ericmarcellini <delacresta@gmail.com>
2023-04-27 21:35:53 +0200

    sign up form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './components/Navbar';
+import RegisterForm from './components/RegisterForm';
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
+      {/* <RegisterForm /> */}
     </div>
   );
 }

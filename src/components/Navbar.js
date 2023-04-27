4af745896880a949f3aabb2d02971ba85ccfb6f5
ericmarcellini <delacresta@gmail.com>
2023-04-27 21:24:47 +0200

    Navbar button removal, added Sign in button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,34 +7,22 @@ import Button from 'react-bootstrap/Button';
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="secondary" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand href="#home">Cool Apparel</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
             <Form className="d-flex">
             <Form.Control
               type="search"
-              placeholder="Search"
+              placeholder="Search for anything"
               className="me-2"
               aria-label="Search"
             />
             </Form>
-            <Button variant="outline-success">Search</Button>
+            <Button variant="success">Search</Button>
+            <Button variant="primary">Sign in</Button>
           </Nav>
         </Navbar.Collapse>
       </Container>

e2c989ecfe1d00708621b1454a1eb4bb25bdc36f
ericmarcellini <delacresta@gmail.com>
2023-04-27 21:09:20 +0200

    deleted the extra buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import NavBar from './components/Navbar';
-import Button from 'react-bootstrap/Button';
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <Button>sup</Button>
-      <h1> Hello World</h1>
     </div>
   );
 }

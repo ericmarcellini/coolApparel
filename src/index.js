7fcf6ea11ba64d71cb95a3a3a6b72738bebf1985
ericmarcellini <delacresta@gmail.com>
2023-11-10 00:19:47 +0100

    working version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
+import App from "./App";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
+  rootElement
 );

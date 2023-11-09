8773db9bbeacffa93b2139a8fd543620755246ad
ericmarcellini <delacresta@gmail.com>
2023-11-09 22:47:14 +0100

    Mainview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/Navbar';
 import RegisterForm from './components/RegisterForm';
+import MainView from './components/MainView';
 
 const App = () => {
   return (
     <div className="App">
-      <NavBar />
-      {/* <RegisterForm /> */}
+      {/*<NavBar /> */}
+      {/*<RegisterForm />*/}
+      <MainView />
     </div>
   );
 }
---
+++ src/components/MainView.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import NavBar from './Navbar';
+
+const MainView = () => {
+    return(
+        <div className="main-view">
+            <header>
+                {/*<NavBar />*/}
+            </header>
+            <section className="hero">
+                {/* Hero section content, with a headline, image, and call to action button */}
+            </section>
+
+            <section className="featured-products">
+                {/* Featured products or promotions */}
+            </section>
+
+            <section className="testimonials">
+                {/* Customer testimonials or reviews */}
+            </section>
+
+            <section className="newsletter">
+                {/* Newsletter signup form */}
+            </section>
+
+            <footer>
+                {/* Footer content with links to other parts of your website */}
+            </footer>
+        </div>
+    )
+}
+
+export default MainView;
